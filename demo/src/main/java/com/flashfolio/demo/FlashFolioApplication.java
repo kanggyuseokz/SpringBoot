3d1b61651b125975f2146377bd0d15179537ac18
kanggyuseokz <gnlvkfka1592@naver.com>
2025-12-09 14:13:38 +0900

    initial commit

---
+++ demo/src/main/java/com/flashfolio/demo/FlashFolioApplication.java
@@ -0,0 +1,13 @@
+package com.flashfolio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashFolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashFolioApplication.class, args);
+	}
+
+}
