ed1e2305b7df870a4b836ae4d18e5fc07064fb95
kanggyuseokz <gnlvkfka1592@naver.com>
2025-12-11 00:45:07 +0900

    SpringBoot Project Initial Commit

---
+++ flashfolio/src/main/java/com/flashfolio/FlashfolioApplication.java
@@ -0,0 +1,13 @@
+package com.flashfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashfolioApplication.class, args);
+	}
+
+}
---
+++ flashfolio/src/test/java/com/flashfolio/FlashfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
